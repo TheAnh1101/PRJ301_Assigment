e81dbf0843b9ebc12181f3bfe379a25e7b069394
TheAnh1101 <hatheanh8a@gmail.com>
2024-07-02 17:50:39 +0700

    Create Model
    Create model: Student, Lecturer, Course, Subject, Assessment, Exam, Grade, Semester

---
+++ PRJ301_Assigment/src/java/model/Lecturer.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+/**
+ *
+ * @author AD
+ */
+public class Lecturer {
+    private int id;
+    private String name;
+    private String email;
+    private Date dob;
+    private boolean gender;
+    private ArrayList<Course> courses = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public ArrayList<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(ArrayList<Course> courses) {
+        this.courses = courses;
+    }
+    
+}
