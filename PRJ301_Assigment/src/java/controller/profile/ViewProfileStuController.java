adb4f44b31f1ac9b071649f1a5d0a56a2a2e0160
TheAnh1101 <hatheanh8a@gmail.com>
2024-07-15 00:48:12 +0700

    Create ViewProfileStuController.java
    Method to get Information about the currently logged Student

---
+++ PRJ301_Assigment/src/java/controller/profile/ViewProfileStuController.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller.profile;
+
+import dal.UserDBContext;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.User;
+
+/**
+ *
+ * @author AD
+ */
+public class ViewProfileStuController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int sid = Integer.parseInt(request.getParameter("sid"));
+        UserDBContext db = new UserDBContext();
+        User loggedUser = (User) request.getSession().getAttribute("user");
+
+        if (loggedUser != null && loggedUser.getStudent().getId() == sid) {
+            User user = db.getInforStudent(sid);
+            if (user != null) {
+                request.setAttribute("user", user);
+                request.getRequestDispatcher("../view/profile/profileStudent.jsp").forward(request, response);
+            } else {
+                request.getRequestDispatcher("../view/error/notExits.jsp").forward(request, response);
+            }
+        } else {
+            response.sendRedirect("../view/error/accessDenied.jsp");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
