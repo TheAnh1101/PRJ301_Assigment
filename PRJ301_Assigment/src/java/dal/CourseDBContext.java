ba163cc07457b9603cde945462cdffb7c1159da0
TheAnh1101 <hatheanh8a@gmail.com>
2024-07-16 04:17:05 +0700

    Update CourseDBContext
    add method filterCourse by Student ID

--- PRJ301_Assigment/src/java/dal/CourseDBContext.java
+++ PRJ301_Assigment/src/java/dal/CourseDBContext.java
@@ -11,6 +11,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.Lecturer;
 import model.Semester;
+import model.Student;
 import model.Subject;
 
 /**
@@ -44,7 +45,56 @@ public class CourseDBContext extends DBContext<Course> {
                 sub.setId(rs.getInt("subid"));
                 sub.setName(rs.getString("subname"));
                 c.setSubject(sub);
+
+                Semester sem = new Semester();
+                sem.setId(rs.getInt("semid"));
+                c.setSemester(sem);
+
+                courses.add(c);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(CourseDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                stm.close();
+                connection.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(CourseDBContext.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        return courses;
+    }
+
+    public ArrayList<Course> filterByStudentID(int sid) {
+        ArrayList<Course> courses = new ArrayList<>();
+        PreparedStatement stm = null;
+        try {
+            String sql = "SELECT c.cid,c.cname,c.subid,sub.subname,c.lid,sc.sid, s.semid,s.season,s.active,s.year FROM\n"
+                    + "                    courses c INNER JOIN semester s ON s.semid = c.semid\n"
+                    + "                    INNER JOIN subjects sub ON sub.subid = c.subid\n"
+                    + "					INNER JOIN students_courses sc ON sc.cid = c.cid\n"
+                    + "                    WHERE s.active = 1 AND sc.sid =  ?";
+
+            stm = connection.prepareStatement(sql);
+            stm.setInt(1, sid);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Course c = new Course();
+                c.setId(rs.getInt("cid"));
+                c.setName(rs.getString("cname"));
+
+                Lecturer l = new Lecturer();
+                l.setId(rs.getInt("lid"));
+                c.setLecturer(l);
                 
+                
+                Subject sub = new Subject();
+                sub.setId(rs.getInt("subid"));
+                sub.setName(rs.getString("subname"));
+                c.setSubject(sub);
+
                 Semester sem = new Semester();
                 sem.setId(rs.getInt("semid"));
                 c.setSemester(sem);
